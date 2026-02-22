17837aa7be707735626eb20a7432f4d623220193
Felipe Lemes <fepalemes@gmail.com>
2026-02-22 19:33:21 -0300

    remover comentários

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   
   },
